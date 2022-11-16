22450cce25b1624cc44dec56dcf1a2584c155abb
Dmitriy Emelyanov <harnblaze@gmail.com>
2022-11-17 01:14:36 +0300

    add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,33 @@
-import React, { FC } from 'react';
-import classes from './Header.module.scss';
+import React, { FC, useState } from 'react';
+import styles from './Header.module.scss';
 
 const Header: FC = () => {
-  return <div className={classes.div}>Header</div>;
+  const [isAuth] = useState(false);
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h2 className={styles.logo}>MOZON</h2>
+        <div className={styles.wrapper}>
+          <input
+            className={styles.search}
+            placeholder="Искать на Mozon"
+            type="text"
+            name="text"
+            autoCapitalize="off"
+            autoComplete="off"
+            autoCorrect="off"
+            spellCheck="false"
+          />
+          <button className={styles.buttonSearch}>Найти</button>
+        </div>
+        {isAuth ? (
+          <button className={styles.button}>Зарегистрироваться</button>
+        ) : (
+          <button className={styles.button}>Войти</button>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
