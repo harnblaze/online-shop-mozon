ed0a3fa6fa6662b924c4e6948b73110f8ecb360c
Dmitriy Emelyanov <harnblaze@gmail.com>
2022-11-16 23:16:52 +0300

    start project

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import classes from './Header.module.scss';
+
+const Header: FC = () => {
+  return <div className={classes.div}>Header</div>;
+};
+
+export default Header;
