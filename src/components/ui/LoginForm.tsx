b7f56cbc3f19063018e095c8d70967832763bdcf
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-05-02 11:28:32 +0300

    add login form

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -1,7 +1,81 @@
 import React, { FC } from 'react';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { Button, Form } from 'react-bootstrap';
 
 const LoginForm: FC = () => {
-  return <div>LOGIN</div>;
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required('Вы должны ввести  email')
+      .email('email не верный'),
+    password: Yup.string().required('Вы должны ввести пароль'),
+  });
+  const formOptions = { resolver: yupResolver(validationSchema) };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm(formOptions);
+
+  const onSubmit = (values: any): void => {
+    console.log('Values:::', values);
+    console.log('Values:::', JSON.stringify(values));
+  };
+
+  const onError = (error: any): void => {
+    console.log('ERROR:::', error);
+  };
+
+  return (
+    <Form
+      onSubmit={() => handleSubmit(onSubmit, onError)}
+      className={'d-flex flex-column'}
+    >
+      <Form.Label as={'h5'} className={'text-center'}>
+        Вход
+      </Form.Label>
+
+      <Form.Group controlId="formBasicEmail" className={'mb-3'}>
+        <>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Введите email"
+            aria-invalid={errors.email !== undefined ? 'true' : 'false'}
+            {...register('email')}
+          />
+          {errors.email !== undefined && (
+            <Form.Text className="text-danger">
+              {errors.email?.message as string}
+            </Form.Text>
+          )}
+        </>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword" className={'mb-3'}>
+        <>
+          <Form.Label>Пароль</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Введите пароль"
+            aria-invalid={errors.password !== undefined ? 'true' : 'false'}
+            {...register('password')}
+          />
+          {errors.password !== undefined && (
+            <Form.Text className="text-danger">
+              {errors.password?.message as string}
+            </Form.Text>
+          )}
+        </>
+      </Form.Group>
+
+      <Button variant="primary" type="submit" className={'mx-auto'}>
+        Войти
+      </Button>
+    </Form>
+  );
 };
 
 export default LoginForm;
