e7c0a681838ed4e6383b9f24a74caf1cf56d8d71
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-05-05 22:17:53 +0300

    fix auth route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,122 +1,132 @@
 const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator");
-const router = express.Router({ mergeParams: true });
+const {check, validationResult} = require("express-validator");
+const router = express.Router({mergeParams: true});
 const tokenService = require("../services/token.service");
 
+function isTokenInvalid(data, dbToken) {
+    console.log(data, dbToken)
+    return !data || !dbToken || data._id !== dbToken?.user?.toString()
+}
+
 router.post("/signUp", [
-  check("email", "Некорректный email").isEmail(),
-  check("password", "Минимальная длина пароля 8 символов").isLength({ min: 8 }),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty())
-        return res.status(400).json({
-          error: {
-            message: "INVALID_DATA",
-            code: 400,
-          },
-        });
-      const { email, password } = req.body;
-      const existingUser = await User.findOne({ email });
+    check("firstName", "Минимальная длина имени 3 символа").isLength({min: 3}),
+    check("lastName", "Минимальная длина фамилии 3 символа").isLength({min: 3}),
+    check("email", "Некорректный email").isEmail(),
+    check("password", "Минимальная длина пароля 8 символов").isLength({min: 8}),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
-      if (existingUser) {
-        return res
-          .status(400)
-          .json({ error: { message: "EMAIL_EXISTS", code: 400 } });
-      }
+            if (!errors.isEmpty())
+                return res.status(400).json({
+                    error: {
+                        message: "INVALID_DATA",
+                        code: 400,
+                    },
+                });
+            const {email, password} = req.body;
+            const existingUser = await User.findOne({email});
 
-      const hashedPassword = await bcrypt.hash(password, 12);
+            if (existingUser) {
+                return res
+                    .status(400)
+                    .json({error: {message: "EMAIL_EXISTS", code: 400}});
+            }
 
-      const newUser = User.create({
-        ...req.body,
-        password: hashedPassword,
-      });
+            const hashedPassword = await bcrypt.hash(password, 12);
 
-      const tokens = tokenService.generate({ _id: newUser._id });
-      await tokenService.save(newUser, tokens.refreshToken);
+            const newUser = await User.create({
+                ...req.body,
+                password: hashedPassword,
+            });
 
-      res.status(201).send({ ...tokens, userId: newUser._id });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: "НА сервере произошла ошибка. Попробуйте позже" });
-    }
-  },
+            const tokens = tokenService.generate({_id: newUser._id});
+            await tokenService.save(newUser, tokens.refreshToken);
+
+            return res.status(201).send({...tokens, userId: newUser._id});
+        } catch (e) {
+            console.log(e)
+            res
+                .status(500)
+                .json({message: "НА сервере произошла ошибка. Попробуйте позже"});
+        }
+    },
 ]);
 
 router.post("/signInWithPassword", [
-  check("email", "Некорректный email").normalizeEmail().isEmail(),
-  check("password", "Пароль не может быть пустым").exists(),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
+    check("email", "Некорректный email").normalizeEmail().isEmail(),
+    check("password", "Пароль не может быть пустым").exists(),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
-      if (!errors.isEmpty())
-        return res.status(400).json({
-          error: {
-            message: "INVALID_DATA",
-            code: 400,
-          },
-        });
+            if (!errors.isEmpty())
+                return res.status(400).json({
+                    error: {
+                        message: "INVALID_DATA",
+                        code: 400,
+                    },
+                });
 
-      const { email, password } = req.body;
-      const existingUser = User.findOne({ email });
-      if (!existingUser) {
-        return res.status(400).send({
-          error: {
-            message: "EMAIL_NOT_FOUND",
-            code: 400,
-          },
-        });
-      }
+            const {email, password} = req.body;
+            const existingUser = await User.findOne({email});
+            if (!existingUser) {
+                return res.status(400).send({
+                    error: {
+                        message: "EMAIL_NOT_FOUND",
+                        code: 400,
+                    },
+                });
+            }
 
-      const isPasswordEqual = await bcrypt.compare(
-        password,
-        existingUser.password
-      );
-      if (!isPasswordEqual) {
-        return res.status(400).send({
-          error: {
-            message: "INVALID_PASSWORD",
-            code: 400,
-          },
-        });
-      }
+            const isPasswordEqual = await bcrypt.compare(
+                password,
+                existingUser.password
+            );
+            if (!isPasswordEqual) {
+                return res.status(400).send({
+                    error: {
+                        message: "INVALID_PASSWORD",
+                        code: 400,
+                    },
+                });
+            }
 
-      const tokens = tokenService.generate({ _id: existingUser._id });
-      await tokens.save(existingUser._id, tokens.refreshToken);
-      res.status(200).send({ ...tokens, userId: existingUser._id });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: "НА сервере произошла ошибка. Попробуйте позже" });
-    }
-  },
+            const tokens = tokenService.generate({_id: existingUser._id});
+            await tokenService.save(existingUser._id, tokens.refreshToken);
+            res.status(200).send({...tokens, userId: existingUser._id});
+        } catch (e) {
+            console.log(e)
+            res
+                .status(500)
+                .json({message: "НА сервере произошла ошибка. Попробуйте позже"});
+        }
+    },
 ]);
 
-router.post("/token", async (req, res) => {
-  try {
-    const { refresh_token: refreshToken } = req.body;
-    tokenService.validateRefresh(refreshToken);
-    const dbToken = tokenService.findToken(refreshToken);
+router.post('/token', async (req, res) => {
+    try {
+        const {refresh_token: refreshToken} = req.body
+        const data = tokenService.validateRefresh(refreshToken)
+        const dbToken = await tokenService.findToken(refreshToken)
 
-    if (!data || !dbToken || data._id === dbToken?.user?.toString()) {
-      return res.status(401).json({ message: "Unauthorized" });
+        if (isTokenInvalid(data, dbToken)) {
+            return res.status(401).json({message: 'Unauthorized'})
+        }
+
+        const tokens = await tokenService.generate({
+            _id: data._id
+        })
+        await tokenService.save(data._id, tokens.refreshToken)
+
+        res.status(200).send({...tokens, userId: data._id})
+    } catch (e) {
+        res.status(500).json({
+            message: 'На сервере произошла ошибка. Попробуйте позже'
+        })
     }
-
-    const tokens = tokenService.generate({
-      _id: data._id,
-    });
-    await tokenService.save(data._id, tokens.refreshToken);
-
-    res.status(200).send({ ...tokens, userId: data._id });
-  } catch (e) {
-    res
-      .status(500)
-      .json({ message: "НА сервере произошла ошибка. Попробуйте позже" });
-  }
-});
+})
 
 module.exports = router;
--- server/services/token.service.js
+++ server/services/token.service.js
@@ -3,46 +3,46 @@ const config = require("config");
 const Token = require("../models/Token");
 
 class TokenService {
-  generate(payload) {
-    const accessToken = jwt.sign(payload, config.get("accessSecret"), {
-      expiresIn: "1h",
-    });
-    const refreshToken = jwt.sign(payload, config.get("refreshSecret"));
-    return { accessToken, refreshToken, expiresIn: 3600 };
-  }
-
-  async save(user, refreshToken) {
-    const data = await Token.findOne({ user });
-    if (data) {
-      data.refreshToken = refreshToken;
-      return data.save();
+    generate(payload) {
+        const accessToken = jwt.sign(payload, config.get("accessSecret"), {
+            expiresIn: "1h",
+        });
+        const refreshToken = jwt.sign(payload, config.get("refreshSecret"));
+        return {accessToken, refreshToken, expiresIn: 3600};
     }
-    return Token.create({ user, refreshToken });
-  }
 
-  validateRefresh(refreshToken) {
-    try {
-      return jwt.verify(refreshToken, config.get("refreshSecret"));
-    } catch (e) {
-      return null;
+    async save(user, refreshToken) {
+        const data = await Token.findOne({user});
+        if (data) {
+            data.refreshToken = refreshToken;
+            return data.save();
+        }
+        return Token.create({user, refreshToken});
     }
-  }
 
-  validateAccess(accessToken) {
-    try {
-      return jwt.verify(accessToken, config.get("accessSecret"));
-    } catch (e) {
-      return null;
+    validateRefresh(refreshToken) {
+        try {
+            return jwt.verify(refreshToken, config.get("refreshSecret"));
+        } catch (e) {
+            return null;
+        }
     }
-  }
 
-  async findToken(refreshToken) {
-    try {
-      return await Token.findOne(refreshToken);
-    } catch (e) {
-      return null;
+    validateAccess(accessToken) {
+        try {
+            return jwt.verify(accessToken, config.get("accessSecret"));
+        } catch (e) {
+            return null;
+        }
     }
-  }
+
+    async findToken(refreshToken) {
+        try {
+            return await Token.findOne({refreshToken})
+        } catch (e) {
+            return null
+        }
+    }
 }
 
 module.exports = new TokenService();
